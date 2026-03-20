3cf83f81ea4c052c4e2c874c9e97a46f2b92c79e
phaANTom2303 <anikavy.goenka@gmail.com>
2026-03-20 12:23:16 +0530

    implemented cache hydration logic

--- rest-api-server/services/ai-verdict.js
+++ rest-api-server/services/ai-verdict.js
@@ -1,8 +1,34 @@
-import  pool  from "#config/db.js";
+import pool from "#config/db.js";
+import { getRedisClient } from "#config/redis.js";
+import { CacheKeys, CACHE_TTL } from "#utils/cache-keys.js";
 class AI_Verdict {
+    static async hydrateRedisWithLatestDecisions() {
+        const redisClient = getRedisClient();
+
+        console.log("Hydrating Redis with latest AI Verdicts...");
+
+        const query = `
+                SELECT DISTINCT ON (company_symbol) company_symbol, trade_date, decision, analysis_id 
+                FROM final_trading_decisions 
+                ORDER BY company_symbol, trade_date DESC;
+            `;
+        const result = await pool.query(query);
+
+        const pipeline = redisClient.multi();
+
+        for (const row of result.rows) {
+
+            const cacheKey = CacheKeys.AI_VERDICT.latest_decision(row.company_symbol);
+            pipeline.set(cacheKey, JSON.stringify(row), { EX: CACHE_TTL.ONE_DAY });
+        }
+
+        await pipeline.exec();
+        console.log(`Successfully hydrated ${result.rowCount} company verdicts.`);
+        return;
+    }
     static async getLatestVerdict(company_symbol) {
         const query =
-        `
+            `
         SELECT company_symbol, trade_date, decision, analysis_id 
         FROM final_trading_decisions 
         WHERE company_symbol=$1
@@ -11,13 +37,13 @@ class AI_Verdict {
         `;
 
         const result = await pool.query(query, [company_symbol]);
-        
+
         return result.rows[0];
     }
 
-    static async getReportsByAnalysisID(analysis_id){
-        const query = 
-        `
+    static async getReportsByAnalysisID(analysis_id) {
+        const query =
+            `
         SELECT agent_name, report_date, report
         FROM agent_reports
         WHERE analysis_id=$1
